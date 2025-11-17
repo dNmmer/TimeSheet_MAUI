d5671d894f1ded90ec06396819eae08cb9f3e435
dNmmer <mail@dnmer.ru>
2025-11-17 23:56:56 +0300

    Initial MAUI project

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TimeSheet_MAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TimeSheet_MAUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
